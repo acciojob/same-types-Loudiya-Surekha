d7ea69b87387f84b4af70f641a97e2d253722333
Loudiya Surekha <lsurekha2002@gmail.com>
2025-05-31 14:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function isSameType(value1, value2) {
 
   // Case 4: Fallback to typeof (if both are strings that failed to convert)
   return typeof value1 === typeof value2;
-}
+} 
 
 // do not change the code below.
-// let value2 = prompt("Enter End Of the Range.");
-// let value1 = prompt("Enter Start of the Range.");
-// alert(isSameType(value1, value2));
+let value2 = prompt("Enter End Of the Range.");
+let value1 = prompt("Enter Start of the Range.");
+alert(isSameType(value1, value2));
